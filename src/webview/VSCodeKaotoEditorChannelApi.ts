18fb114987f5261f4650f6bd9650ecf11e0e5c10
Aurélien Pupier <apupier@redhat.com>
2024-09-13 14:59:46 +0200

    WIP
    first manually check version, working for several happy paths

--- src/webview/VSCodeKaotoEditorChannelApi.ts
+++ src/webview/VSCodeKaotoEditorChannelApi.ts
@@ -113,9 +113,14 @@ export class VSCodeKaotoEditorChannelApi extends DefaultVsCodeKieEditorChannelAp
       const targetFile = path.resolve(path.dirname(this.currentEditedDocument.uri.fsPath), relativePath);
       return new TextDecoder().decode(await vscode.workspace.fs.readFile(vscode.Uri.file(targetFile)));
     } catch (ex) {
+<<<<<<< Upstream, based on origin/main
       const errorMessage= `Cannot retrieve content of ${relativePath} relatively to ${this.currentEditedDocument.uri.fsPath}`;
       vscode.window.showErrorMessage(errorMessage);
       logInKaotoOutputChannel(errorMessage, ex);
+=======
+      vscode.window.showErrorMessage(`Cannot retrieve content of ${relativePath} relatively to ${this.currentEditedDocument.uri.fsPath}`);
+      // TODO log the exception somewhere
+>>>>>>> 2f0512b WIP
       return undefined;
     }
   }
@@ -125,6 +130,7 @@ export class VSCodeKaotoEditorChannelApi extends DefaultVsCodeKieEditorChannelAp
       const targetFile = path.resolve(path.dirname(this.currentEditedDocument.uri.fsPath), relativePath);
       await vscode.workspace.fs.writeFile(vscode.Uri.file(targetFile), new TextEncoder().encode(content));
     } catch (ex) {
+<<<<<<< Upstream, based on origin/main
       const errorMessage = `Cannot write content of ${relativePath} relatively to ${this.currentEditedDocument.uri.fsPath}`;
       vscode.window.showErrorMessage(errorMessage);
       logInKaotoOutputChannel(errorMessage, ex);
@@ -165,6 +171,10 @@ export class VSCodeKaotoEditorChannelApi extends DefaultVsCodeKieEditorChannelAp
       const errorMessage = `Cannot get a user selection: ${ex.message}`;
       vscode.window.showErrorMessage(errorMessage);
       logInKaotoOutputChannel(errorMessage, ex);
+=======
+      vscode.window.showErrorMessage(`Cannot write content of ${relativePath} relatively to ${this.currentEditedDocument.uri.fsPath}`);
+      // TODO log the exception somewhere
+>>>>>>> 2f0512b WIP
       return undefined;
     }
   }
